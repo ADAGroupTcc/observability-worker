38c24cf63988cbd2d0d39690a4960e89b14d1511
RafalFDS2 <rafael.ferraz@picpay.com>
2024-04-28 16:38:11 -0300

    testes

--- main.go
+++ main.go
@@ -11,47 +11,56 @@ import (
 	"worker/config"
 )
 
+var envs *config.Environments
+
 func main() {
-	envs, err := config.LoadEnvVars()
+	var err error
+	envs, err = config.LoadEnvVars()
 	if err != nil {
 		log.Fatalf("config.LoadEnvVars: %v", err)
 	}
-	cl := http.Client{}
-	go func() {
-		http.HandleFunc("/health", HealthHandler)
-		if err := http.ListenAndServe(fmt.Sprintf(":%s", envs.ApiPort), nil); err != nil {
-			log.Fatal(fmt.Sprintf("Failed to listen port %s", envs.ApiPort), err)
-			os.Exit(1)
-		}
-	}()
-	var wg sync.WaitGroup
-	for {
-		log.Default().Printf("Start checking APIs healthiness")
-		for _, baseUrl := range envs.ApiBaseUrls {
-			wg.Add(1)
-			go func(baseUrl string) {
-				defer wg.Done()
-				url, _ := url.Parse(fmt.Sprintf("%s/health", baseUrl))
-				req := &http.Request{Method: "GET", URL: url, Header: http.Header{"Api_Token": []string{envs.ApiToken}}}
-				res, err := cl.Do(req)
-				if err != nil {
-					log.Fatal(err)
-					log.Fatalf("API %s is unhealthy", baseUrl)
-				}
-				if res.StatusCode == 200 {
-					log.Printf("API %s is healthy", baseUrl)
-				} else {
-					log.Fatalf("API %s is unhealthy", baseUrl)
-				}
-			}(baseUrl)
-		}
-		wg.Wait()
-		log.Default().Printf("Finish checking APIs healthiness")
-		time.Sleep(time.Duration(envs.PollingInterval) * time.Second)
+	http.HandleFunc("/health", HealthHandler)
+	http.HandleFunc("/check", CheckHelthinessHandler)
+	if err := http.ListenAndServe(fmt.Sprintf(":%s", envs.ApiPort), nil); err != nil {
+		log.Fatal(fmt.Sprintf("Failed to listen port %s", envs.ApiPort), err)
+		os.Exit(1)
 	}
 }
 
 func HealthHandler(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
+	w.Write([]byte(`{"status":"ok"}`))
+}
+
+func CheckHelthinessHandler(w http.ResponseWriter, r *http.Request) {
+	go func() {
+		cl := http.Client{}
+		var wg sync.WaitGroup
+		for {
+			log.Default().Printf("Start checking APIs healthiness")
+			for _, baseUrl := range envs.ApiBaseUrls {
+				wg.Add(1)
+				go func(baseUrl string) {
+					defer wg.Done()
+					url, _ := url.Parse(fmt.Sprintf("%s/health", baseUrl))
+					req := &http.Request{Method: "GET", URL: url, Header: http.Header{"Api_Token": []string{envs.ApiToken}}}
+					res, err := cl.Do(req)
+					if err != nil {
+						log.Fatal(err)
+						log.Fatalf("API %s is unhealthy", baseUrl)
+					}
+					if res.StatusCode == 200 {
+						log.Printf("API %s is healthy", baseUrl)
+					} else {
+						log.Fatalf("API %s is unhealthy", baseUrl)
+					}
+				}(baseUrl)
+			}
+			wg.Wait()
+			log.Default().Printf("Finish checking APIs healthiness")
+			time.Sleep(time.Duration(envs.PollingInterval) * time.Second)
+		}
+	}()
 	w.Header().Set("Content-Type", "application/json")
 	w.Write([]byte(`{"status":"ok"}`))
 }
